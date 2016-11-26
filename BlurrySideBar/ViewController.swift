e622b1f03785ce4e9ae251e193d3499dc25867c6
Rey Cerio <reycerio@Reys-MacBook-Pro.local>
2016-11-25 18:54:02 -0800

    Initial Commit

---
+++ BlurrySideBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlurrySideBar
+//
+//  Created by Rey Cerio on 2016-11-25.
+//  Copyright © 2016 CeriOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
